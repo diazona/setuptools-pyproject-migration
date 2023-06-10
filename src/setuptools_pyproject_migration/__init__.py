7ef3d1d498f8de0e194283f1de79ac9f2fb92dc9
David Zaslavsky <diazona@ellipsix.net>
2023-06-10 00:22:40 -0700

    Add a basic setuptools command that prints the package name and version
    This is a proof-of-concept for taking the package metadata collected by
    setuptools and printing it out as a bare-bones pyproject.toml file.
    Right now it only looks at the name and version (and it only detects
    the static version number, without regard for setuptools_scm or anything
    that would dynamically determine the version number). We'll fill in this
    command class with proper detection for more metadata in the future.

---
+++ src/setuptools_pyproject_migration/__init__.py
@@ -0,0 +1,28 @@
+import setuptools
+import sys
+import tomlkit
+
+
+class WritePyproject(setuptools.Command):
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        dist: setuptools.dist.Distribution = self.distribution
+
+        pyproject = {
+            "build-system": {
+                "requires": ["setuptools"],
+                "build-backend": "setuptools.build_meta",
+            }
+        }
+
+        pyproject["project"] = {
+            "name": dist.get_name(),
+            "version": dist.get_version(),  # TODO try to reverse-engineer dynamic version
+        }
+
+        tomlkit.dump(pyproject, sys.stdout)
