7c79d371e7c9508e8b4754f6bcbe2d12e2febdcf
Stuart Longland <me@vk4msl.com>
2023-07-08 18:56:20 +1000

    setup command tests: Add tests for dependency pass-through.

--- tests/test_setup_command.py
+++ tests/test_setup_command.py
@@ -1,4 +1,7 @@
 def test_name_and_version(project) -> None:
+    """
+    Test we can generate a basic project skeleton.
+    """
     setup_cfg = """\
 [metadata]
 name = test-project
@@ -7,6 +10,140 @@ version = 0.0.1
     pyproject_toml = """\
 [build-system]
 requires = ["setuptools"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "test-project"
+version = "0.0.1"
+"""
+    project.setup_cfg(setup_cfg)
+    project.setup_py()
+    result = project.run()
+    assert result.returncode == 0
+    assert result.stdout == "running pyproject\n" + pyproject_toml
+
+
+# install_requires tests, we use made-up module names here.
+
+
+def test_install_requires(project) -> None:
+    """
+    Test the install_requires is passed through if given.
+    """
+    setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+
+[options]
+install_requires =
+        dependency1
+        dependency2>=1.23
+        dependency3<4.56
+"""
+    pyproject_toml = """\
+[build-system]
+requires = ["setuptools"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "test-project"
+version = "0.0.1"
+dependencies = ["dependency1", "dependency2>=1.23", "dependency3<4.56"]
+"""
+    project.setup_cfg(setup_cfg)
+    project.setup_py()
+    result = project.run()
+    assert result.returncode == 0
+    assert result.stdout == "running pyproject\n" + pyproject_toml
+
+
+# setup_requires tests: we have to use real dependencies that actually are
+# installed, otherwise the tests will fail.
+
+
+def test_setup_requires(project) -> None:
+    """
+    Test setup_requires is passed through with 'setuptools' dependency.
+    """
+    setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+
+[options]
+setup_requires =
+        sphinx
+        pytest>=6
+        pytest-black<99.88.77
+"""
+    pyproject_toml = """\
+[build-system]
+requires = ["pytest-black<99.88.77", "pytest>=6", "setuptools", "sphinx"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "test-project"
+version = "0.0.1"
+"""
+    project.setup_cfg(setup_cfg)
+    project.setup_py()
+    result = project.run()
+    assert result.returncode == 0
+    assert result.stdout == "running pyproject\n" + pyproject_toml
+
+
+def test_setup_requires_setuptools(project) -> None:
+    """
+    Test that we don't duplicate 'setuptools' in build requirements
+    """
+    setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+
+[options]
+setup_requires =
+        setuptools
+        sphinx
+        pytest>=6
+        pytest-black<99.88.77
+"""
+    pyproject_toml = """\
+[build-system]
+requires = ["pytest-black<99.88.77", "pytest>=6", "setuptools", "sphinx"]
+build-backend = "setuptools.build_meta"
+
+[project]
+name = "test-project"
+version = "0.0.1"
+"""
+    project.setup_cfg(setup_cfg)
+    project.setup_py()
+    result = project.run()
+    assert result.returncode == 0
+    assert result.stdout == "running pyproject\n" + pyproject_toml
+
+
+def test_setup_requires_setuptools_version(project) -> None:
+    """
+    Test we can handle a build system that requires a specific setuptools version.
+    """
+    setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+
+[options]
+setup_requires =
+        setuptools>=34.56
+        sphinx
+        pytest>=6
+        pytest-black<99.88.77
+"""
+    pyproject_toml = """\
+[build-system]
+requires = ["pytest-black<99.88.77", "pytest>=6", "setuptools>=34.56", "sphinx"]
 build-backend = "setuptools.build_meta"
 
 [project]
