c88413926d76ec68db9e27a4d1dfea295b035f97
Stuart Longland <me@vk4msl.com>
2023-07-08 17:53:09 +1000

    __init__: Implement build-system.build-backend and project.dependencies
    This hopefully should copy across the dependencies from the original
    project `setup.py`.
    
    Omit project dependencies if none are specified.

--- src/setuptools_pyproject_migration/__init__.py
+++ src/setuptools_pyproject_migration/__init__.py
@@ -1,6 +1,7 @@
 import setuptools
 import sys
 import tomlkit
+from pep508_parser import parser as pep508
 from typing import List, Optional, Tuple
 
 
@@ -17,9 +18,28 @@ class WritePyproject(setuptools.Command):
     def run(self):
         dist: setuptools.dist.Distribution = self.distribution
 
+        # pyproject.toml schema:
+        # https://packaging.python.org/en/latest/specifications/declaring-project-metadata/#declaring-project-metadata
+
+        # Enumerate all set-up and build requirements, ensure there are no duplicates
+        setup_requirements = set(dist.setup_requires)
+
+        # Is 'setuptools' already there?
+        seen_setuptools = False
+        for dep in setup_requirements:
+            (dep_module, _, _, _) = pep508.parse(dep)
+            if dep_module == "setuptools":
+                # Yep, here it is!
+                seen_setuptools = True
+                break
+
+        if not seen_setuptools:
+            # We will need it here
+            setup_requirements.add("setuptools")
+
         pyproject = {
             "build-system": {
-                "requires": ["setuptools"],
+                "requires": list(sorted(setup_requirements)),
                 "build-backend": "setuptools.build_meta",
             }
         }
@@ -28,5 +48,10 @@ class WritePyproject(setuptools.Command):
             "name": dist.get_name(),
             "version": dist.get_version(),  # TODO try to reverse-engineer dynamic version
         }
+
+        # NB: ensure a consistent alphabetical ordering of dependencies
+        dependencies = list(sorted(set(dist.install_requires)))
+        if dependencies:
+            pyproject["project"]["dependencies"] = dependencies
 
         tomlkit.dump(pyproject, sys.stdout)
