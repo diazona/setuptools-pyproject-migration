b8bd0f6dbc5fc888d6a95c5ee9adbb886d14e3bc
David Zaslavsky <diazona@ellipsix.net>
2023-07-16 17:34:02 -0700

    Drop down to two tests that actually run the setup.py command
    This commit changes the set of tests which actually run
    `setup.py pyproject` to be just two: one extremely minimal test which
    just has a name and version, and another comprehensive test that is
    supposed to include all relevant fields that can appear in setup.cfg.
    The idea is that as we add support for more fields, we'll adjust
    the expected output of that test to keep up. I populated the dummy
    setup.cfg file for the latter test with a bunch of fields that exist
    in setuptools' own setup.cfg file as well as this project's setup.cfg.

--- tests/test_setup_command.py
+++ tests/test_setup_command.py
@@ -43,27 +43,105 @@ version = "0.0.1"
     check_result(result, pyproject_toml)
 
 
-# install_requires tests, we use made-up module names here.
+# This next test should be kept up to date as we add support for more fields.
 
 
-def test_install_requires(project) -> None:
+def test_everything(project) -> None:
     """
-    Test the install_requires is passed through if given.
+    Test all fields that the project supports.
     """
     setup_cfg = """\
 [metadata]
 name = test-project
 version = 0.0.1
+author = David Zaslavsky, Stuart Longland
+author_email = diazona@ellipsix.net, me@vk4msl.com
+description = A dummy project with a sophisticated setup.cfg file
+long_description = file:README.md
+url = https://example.com/test-project
+classifiers =
+        Development Status :: 1 - Planning
+        Environment :: Console
+        Intended Audience :: Developers
+        License :: OSI Approved :: MIT License
+        Operating System :: OS Independent
+        Programming Language :: Python
+        Programming Language :: Python :: 3
+        Programming Language :: Python :: 3 :: Only
+        Programming Language :: Python :: 3.6
+        Programming Language :: Python :: 3.7
+        Programming Language :: Python :: 3.8
+        Programming Language :: Python :: 3.9
+        Programming Language :: Python :: 3.10
+        Programming Language :: Python :: 3.11
+        Programming Language :: Python :: 3.12
+        Topic :: Software Development :: Testing
+keywords = setuptools testing
+project_urls =
+        Homepage = https://example.com/test-project
+        Documentation = https://example.com/test-project/docs
 
 [options]
+packages = find:
+package_dir =
+        = src
+include_package_data = true
+python_requires = >= 3.6
 install_requires =
         dependency1
         dependency2>=1.23
         dependency3<4.56
+setup_requires =
+        sphinx
+        pytest>=6
+        pytest-black<99.88.77
+
+[options.packages.find]
+where = src
+exclude =
+        build*
+        dist*
+        docs*
+        tests*
+        *.tests
+        *.tests.*
+        tools*
+
+[options.extras_require]
+testing =
+        # taken from jaraco/skeleton (we just need an arbitrary list of packages)
+        pytest >= 6
+        pytest-checkdocs >= 2.4
+        pytest-black >= 0.3.7; \
+            python_implementation != "PyPy"
+        pytest-cov; \
+            python_implementation != "PyPy"
+        pytest-mypy >= 0.9.1; \
+            python_implementation != "PyPy"
+        pytest-enabler >= 2.2
+        pytest-ruff; sys_platform != "cygwin"
+
+docs =
+        # taken from jaraco/skeleton (we just need an arbitrary list of packages)
+        sphinx >= 3.5
+        jaraco.packaging >= 9
+        rst.linker >= 1.9
+        furo
+        sphinx-lint
+
+[options.entry_points]
+test_project.dummy =
+        ep1 = test_project.ep1
+        ep2 = test_project.ep2
+"""
+    readme_md = """\
+# test-project
+
+Lorem ipsum and all that
 """
     pyproject_toml = """\
 [build-system]
-requires = ["setuptools"]
+requires = ["pytest-black<99.88.77", "pytest>=6", "setuptools", "sphinx"]
 build-backend = "setuptools.build_meta"
 
 [project]
@@ -72,102 +150,7 @@ version = "0.0.1"
 dependencies = ["dependency1", "dependency2>=1.23", "dependency3<4.56"]
 """
     project.setup_cfg(setup_cfg)
-    project.setup_py()
-    result = project.run()
-    check_result(result, pyproject_toml)
-
-
-# setup_requires tests: we have to use real dependencies that actually are
-# installed, otherwise the tests will fail.
-
-
-def test_setup_requires(project) -> None:
-    """
-    Test setup_requires is passed through with 'setuptools' dependency.
-    """
-    setup_cfg = """\
-[metadata]
-name = test-project
-version = 0.0.1
-
-[options]
-setup_requires =
-        sphinx
-        pytest>=6
-        pytest-black<99.88.77
-"""
-    pyproject_toml = """\
-[build-system]
-requires = ["pytest-black<99.88.77", "pytest>=6", "setuptools", "sphinx"]
-build-backend = "setuptools.build_meta"
-
-[project]
-name = "test-project"
-version = "0.0.1"
-"""
-    project.setup_cfg(setup_cfg)
-    project.setup_py()
-    result = project.run()
-    check_result(result, pyproject_toml)
-
-
-def test_setup_requires_setuptools(project) -> None:
-    """
-    Test that we don't duplicate 'setuptools' in build requirements
-    """
-    setup_cfg = """\
-[metadata]
-name = test-project
-version = 0.0.1
-
-[options]
-setup_requires =
-        setuptools
-        sphinx
-        pytest>=6
-        pytest-black<99.88.77
-"""
-    pyproject_toml = """\
-[build-system]
-requires = ["pytest-black<99.88.77", "pytest>=6", "setuptools", "sphinx"]
-build-backend = "setuptools.build_meta"
-
-[project]
-name = "test-project"
-version = "0.0.1"
-"""
-    project.setup_cfg(setup_cfg)
-    project.setup_py()
-    result = project.run()
-    check_result(result, pyproject_toml)
-
-
-def test_setup_requires_setuptools_version(project) -> None:
-    """
-    Test we can handle a build system that requires a specific setuptools version.
-    """
-    setup_cfg = """\
-[metadata]
-name = test-project
-version = 0.0.1
-
-[options]
-setup_requires =
-        setuptools>=34.56
-        sphinx
-        pytest>=6
-        pytest-black<99.88.77
-"""
-    pyproject_toml = """\
-[build-system]
-requires = ["pytest-black<99.88.77", "pytest>=6", "setuptools>=34.56", "sphinx"]
-build-backend = "setuptools.build_meta"
-
-[project]
-name = "test-project"
-version = "0.0.1"
-"""
-    project.setup_cfg(setup_cfg)
+    project.write("README.md", readme_md)
     project.setup_py()
     result = project.run()
     check_result(result, pyproject_toml)
