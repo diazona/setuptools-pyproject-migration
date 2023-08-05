9d4f448ce5b146510314ec35d0fc1f041c3da10e
Stuart Longland <me@vk4msl.com>
2023-08-05 19:56:32 +1000

    Add a `dumpmeta` debug command
    Putting this in a separate branch as this is not intended to be shipped
    in the final distribution, but rather is present to enable discovery of
    where the various pieces of metadata hide.

--- src/setuptools_pyproject_migration/__init__.py
+++ src/setuptools_pyproject_migration/__init__.py
@@ -1,8 +1,9 @@
 import setuptools
 import sys
 import tomlkit
+import json
 from pep508_parser import parser as pep508
-from typing import Dict, List, Optional, Tuple, Type, Union
+from typing import Any, Dict, List, Optional, Tuple, Type, Union
 
 # After we drop support for Python <3.10, we can import TypeAlias directly from typing
 from typing_extensions import Required, TypedDict
@@ -42,6 +43,63 @@ Project: Type = TypedDict(
 )
 
 Pyproject: Type = TypedDict("Pyproject", {"build-system": BuildSystem, "project": Project}, total=False)
+
+
+class DumpMetadata(setuptools.Command):  # pragma: no cover
+    """
+    Dump the metadata provided in the setup package.  This is a debugging tool
+    primarily to figure out what fields are exposed and where they may be
+    hiding.
+    """
+    # Note: excluded from coverage, as this is meant as a debugging fixture.
+
+    # Each option tuple contains (long name, short name, help string)
+    user_options: List[Tuple[str, Optional[str], str]] = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        metadata: Dict[str, Dict[str, Any]] = {}
+
+        for attr in dir(self.distribution):
+            if attr.startswith("_"):
+                # Ignore 'protected' members
+                continue
+
+            value: Any = getattr(self.distribution, attr)
+            if hasattr(value, "__call__"):
+                if not attr.startswith("get_"):
+                    # Ignore methods that are not "getters"
+                    continue
+
+                try:
+                    value = value()
+                    metadata.setdefault("methods", {})[attr] = self._to_json(value)
+                except:
+                    continue
+            else:
+                metadata.setdefault("properties", {})[attr] = self._to_json(value)
+
+        print(json.dumps(metadata, indent=4, sort_keys=True))
+
+
+    def _to_json(self, v: Any) -> Any:
+        if (v is None) or isinstance(v, (bool, int, str)):
+            return v
+        elif isinstance(v, list):
+            return [self._to_json(e) for e in v]
+        elif isinstance(v, dict):
+            return dict([
+                (self._to_json(k), self._to_json(e))
+                for k, e
+                in v.items()
+            ])
+        else:
+            return repr(v)
 
 
 class WritePyproject(setuptools.Command):
@@ -101,4 +159,4 @@ class WritePyproject(setuptools.Command):
         tomlkit.dump(self._generate(), sys.stdout)
 
 
-__all__ = ["WritePyproject"]
+__all__ = ["WritePyproject", "DumpMetadata"]
