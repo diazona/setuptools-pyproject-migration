ca1a243454374d6cadf812ece736467314b44bf9
David Zaslavsky <diazona@ellipsix.net>
2023-08-04 23:50:22 -0700

    Add tests of the plugin with author and maintainer metadata
    These tests run various combinations of author and maintainer metadata
    through the plugin. The tests only verify the "typical" cases; I'm
    leaving it to the unit tests for _transform_contributors() to ensure
    that weird cases are handled in the proper way.
    
    I made a judgment call to treat any missing metadata as an empty string,
    but to never generate an author or maintainer entry in pyproject.toml
    unless at least one of the name or email address is nonempty. This was
    an arbitrary choice, at least in the sense that I didn't do any research
    to determine how those cases are supposed to be handled (if there is any
    standard for them). If necessary we can fix up the behavior later.
    
    I wrote these tests to use the project fixture rather than manually
    constructing Distribution objects because if I try the latter way,
    it looks like setuptools_scm takes over and computes the version number
    from the actual setuptools-pyproject-migration project, rather than
    using the hard-coded version 0.0.1, which causes the tests to fail.
    Hopefully in a future commit we can fix that.

---
+++ tests/test_contributors.py
@@ -0,0 +1,251 @@
+"""
+Tests of author and maintainer metadata
+"""
+
+
+class TestAuthors:
+    def test_single_author(self, project) -> None:
+        setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+author = Monty Python
+author_email = python@python.example.com
+"""
+        pyproject = {
+            "build-system": {
+                "requires": ["setuptools"],
+                "build-backend": "setuptools.build_meta",
+            },
+            "project": {
+                "name": "test-project",
+                "version": "0.0.1",
+                "authors": [
+                    {"name": "Monty Python", "email": "python@python.example.com"},
+                ],
+            },
+        }
+        project.setup_cfg(setup_cfg)
+        project.setup_py()
+        result = project.generate()
+        assert result == pyproject
+
+    def test_only_name(self, project) -> None:
+        setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+author = Monty Python
+"""
+        pyproject = {
+            "build-system": {
+                "requires": ["setuptools"],
+                "build-backend": "setuptools.build_meta",
+            },
+            "project": {
+                "name": "test-project",
+                "version": "0.0.1",
+                "authors": [
+                    {"name": "Monty Python", "email": ""},
+                ],
+            },
+        }
+        project.setup_cfg(setup_cfg)
+        project.setup_py()
+        result = project.generate()
+        assert result == pyproject
+
+    def test_only_email(self, project) -> None:
+        setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+author_email = python@python.example.com
+"""
+        pyproject = {
+            "build-system": {
+                "requires": ["setuptools"],
+                "build-backend": "setuptools.build_meta",
+            },
+            "project": {
+                "name": "test-project",
+                "version": "0.0.1",
+                "authors": [
+                    {"name": "", "email": "python@python.example.com"},
+                ],
+            },
+        }
+        project.setup_cfg(setup_cfg)
+        project.setup_py()
+        result = project.generate()
+        assert result == pyproject
+
+    def test_multiple_authors(self, project) -> None:
+        setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+author = John Cleese, Terry Gilliam
+author_email = john@python.example.com, terry-the-second@python.example.com
+"""
+        pyproject = {
+            "build-system": {
+                "requires": ["setuptools"],
+                "build-backend": "setuptools.build_meta",
+            },
+            "project": {
+                "name": "test-project",
+                "version": "0.0.1",
+                "authors": [
+                    {"name": "John Cleese", "email": "john@python.example.com"},
+                    {"name": "Terry Gilliam", "email": "terry-the-second@python.example.com"},
+                ],
+            },
+        }
+        project.setup_cfg(setup_cfg)
+        project.setup_py()
+        result = project.generate()
+        assert result == pyproject
+
+
+class TestMaintainers:
+    def test_single_maintainer(self, project) -> None:
+        setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+maintainer = Monty Python
+maintainer_email = python@python.example.com
+"""
+        pyproject = {
+            "build-system": {
+                "requires": ["setuptools"],
+                "build-backend": "setuptools.build_meta",
+            },
+            "project": {
+                "name": "test-project",
+                "version": "0.0.1",
+                "maintainers": [
+                    {"name": "Monty Python", "email": "python@python.example.com"},
+                ],
+            },
+        }
+        project.setup_cfg(setup_cfg)
+        project.setup_py()
+        result = project.generate()
+        assert result == pyproject
+
+    def test_only_name(self, project) -> None:
+        setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+maintainer = Monty Python
+"""
+        pyproject = {
+            "build-system": {
+                "requires": ["setuptools"],
+                "build-backend": "setuptools.build_meta",
+            },
+            "project": {
+                "name": "test-project",
+                "version": "0.0.1",
+                "maintainers": [
+                    {"name": "Monty Python", "email": ""},
+                ],
+            },
+        }
+        project.setup_cfg(setup_cfg)
+        project.setup_py()
+        result = project.generate()
+        assert result == pyproject
+
+    def test_only_email(self, project) -> None:
+        setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+maintainer_email = python@python.example.com
+"""
+        pyproject = {
+            "build-system": {
+                "requires": ["setuptools"],
+                "build-backend": "setuptools.build_meta",
+            },
+            "project": {
+                "name": "test-project",
+                "version": "0.0.1",
+                "maintainers": [
+                    {"name": "", "email": "python@python.example.com"},
+                ],
+            },
+        }
+        project.setup_cfg(setup_cfg)
+        project.setup_py()
+        result = project.generate()
+        assert result == pyproject
+
+    def test_multiple_maintainers(self, project) -> None:
+        setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+maintainer = John Cleese, Terry Gilliam
+maintainer_email = john@python.example.com, terry-the-second@python.example.com
+"""
+        pyproject = {
+            "build-system": {
+                "requires": ["setuptools"],
+                "build-backend": "setuptools.build_meta",
+            },
+            "project": {
+                "name": "test-project",
+                "version": "0.0.1",
+                "maintainers": [
+                    {"name": "John Cleese", "email": "john@python.example.com"},
+                    {"name": "Terry Gilliam", "email": "terry-the-second@python.example.com"},
+                ],
+            },
+        }
+        project.setup_cfg(setup_cfg)
+        project.setup_py()
+        result = project.generate()
+        assert result == pyproject
+
+
+def test_authors_and_maintainers(project) -> None:
+    """
+    Test a situation where both the author and maintainer fields are set.
+    """
+
+    setup_cfg = """\
+[metadata]
+name = test-project
+version = 0.0.1
+author = Terry Jones, Michael Palin
+author_email = terry-the-first@python.example.com, michael@python.example.com
+maintainer = Graham Chapman, John Cleese
+maintainer_email = graham@python.example.com, john@python.example.com
+"""
+    pyproject = {
+        "build-system": {
+            "requires": ["setuptools"],
+            "build-backend": "setuptools.build_meta",
+        },
+        "project": {
+            "name": "test-project",
+            "version": "0.0.1",
+            "authors": [
+                {"name": "Terry Jones", "email": "terry-the-first@python.example.com"},
+                {"name": "Michael Palin", "email": "michael@python.example.com"},
+            ],
+            "maintainers": [
+                {"name": "Graham Chapman", "email": "graham@python.example.com"},
+                {"name": "John Cleese", "email": "john@python.example.com"},
+            ],
+        },
+    }
+    project.setup_cfg(setup_cfg)
+    project.setup_py()
+    result = project.generate()
+    assert result == pyproject
