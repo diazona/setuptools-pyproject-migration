22639db5f8d316f2a5340eeaa35765e5b3012e38
David Zaslavsky <diazona@ellipsix.net>
2023-08-20 00:26:44 -0700

    Streamline the manual creation of Distribution objects with new fixtures
    When testing WritePyproject, we need to give it a Distribution object,
    which can be created either by calling setuptools.setup() or by manually
    calling the constructor and passing in a dict of attributes. The latter
    way is faster to run because it doesn't have to create and read a file,
    but it's a little unwieldy to write that code over and over again. Plus,
    we've had problems where the Distribution object will read
    setuptools configuration files in whatever is the current working
    directory when the test runs, and will set some of its attributes based
    on that configuration. (Specifically, it seems to use setuptools_scm to
    set the version number, but maybe this would apply to other dynamic
    attributes, I'm not sure.)
    
    This commit adds a fixture, make_write_pyproject, that will address
    the problem in two ways:
    
    - It ensures that any test requesting the fixture will run in an empty
      working directory
    - It also provides a convenience function that creates a Distribution
      from the given keyword arguments and then creates a WritePyproject
      from that Distribution.
    
    We can now use this fixture to avoid having to run the full setuptools
    configuration procedure if we don't have to.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,11 @@ import packaging.version
 import pathlib
 import pytest
 import setuptools
+import setuptools.dist
 import warnings
 from pytest_console_scripts import ScriptRunner
 from setuptools_pyproject_migration import Pyproject, WritePyproject
-from typing import Optional, Union
+from typing import Callable, Iterator, List, Optional, Union
 
 try:
     from importlib.metadata import version as im_version
@@ -155,3 +156,82 @@ def project(tmp_path: pathlib.Path, monkeypatch: pytest.MonkeyPatch, script_runn
     """
     monkeypatch.chdir(tmp_path)
     return Project(tmp_path, script_runner)
+
+
+@pytest.fixture(scope="session")
+def _session_empty_directory(tmp_path_factory: pytest.TempPathFactory) -> pathlib.Path:
+    """
+    Create an empty temporary directory that persists throughout the entire
+    session.
+
+    The directory is returned as a :py:class:`pathlib.Path` which has had
+    its write permission removed. While it's technically possible for a caller
+    to restore write permission and put something in the directory... don't
+    do that.
+    """
+    empty_tmpdir: pathlib.Path = tmp_path_factory.mktemp("empty")
+    # Make the directory read-only to try to prevent accidental creation of files in it.
+    # 0o555 gives read and execute (but not write) access to user, group, and others.
+    empty_tmpdir.chmod(0o555)
+    return empty_tmpdir
+
+
+# This has to be a function-scoped fixture because it relies on monkeypatch
+@pytest.fixture
+def in_empty_directory(
+    _session_empty_directory: pathlib.Path,
+    monkeypatch: pytest.MonkeyPatch,
+    request: pytest.FixtureRequest,
+) -> Iterator[None]:
+    """
+    Change into an empty directory for the duration of the test.
+
+    The empty directory used is one that persists for the entire session and
+    has been set read-only to try to prevent any files from being created in
+    it. Test code should not write anything into the directory.
+    """
+
+    assert "project" not in request.fixturenames, "in_empty_directory fixture conflicts with project fixture"
+
+    # Change into the session's empty directory
+    monkeypatch.chdir(_session_empty_directory)
+
+    # Wait for the test to run
+    yield
+
+    # Make sure that the directory is still empty
+
+    # This is inefficient if a large number of files do get written into
+    # the directory, but we really shouldn't need to optimize for that case
+    dir_contents: List[pathlib.Path] = list(_session_empty_directory.iterdir())
+    # fmt: off
+    assert not dir_contents, \
+        "Files were added to the session's empty directory: " + ", ".join(f.name for f in dir_contents)
+    # fmt: on
+
+
+def _make_write_pyproject(**kwargs) -> WritePyproject:
+    """
+    A convenience function to create a :py:class:`WritePyproject` instance
+    initialized from a :py:class:`setuptools.dist.Distribution` with
+    the given arguments.
+    """
+    return WritePyproject(setuptools.dist.Distribution(kwargs))
+
+
+@pytest.fixture
+def make_write_pyproject(in_empty_directory: None) -> Callable[..., WritePyproject]:
+    """
+    Provide a convenience function to create a :py:class:`WritePyproject`
+    instance initialized from a :py:class:`setuptools.dist.Distribution` with
+    given arguments.
+
+    This fixture also changes into an empty directory to ensure that the created
+    ``Distribution`` instance only takes its attributes from the keyword
+    arguments passed, not from any setuptools configuration files that may exist
+    in the current directory. If you do want a ``Distribution`` object to use
+    files in the current directory as a configuration source, use
+    the :py:func:`project` fixture instead.
+    """
+
+    return _make_write_pyproject
