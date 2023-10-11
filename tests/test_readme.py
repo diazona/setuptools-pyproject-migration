df51f44e9b173c22f3dacb907544085da4c9f715
Stuart Longland <me@vk4msl.com>
2023-10-11 10:09:12 +1000

    issue 102: add failing tests

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -81,6 +81,26 @@ long_description_content_type = {mime_type}
 
 
 @parametrize_readme_type
+def test_file_with_content_type_setuppy(project, extension: str, mime_type: str) -> None:
+    readme_filename = f"README.{extension}"
+    setup_py = f"""\
+import setuptools
+
+setuptools.setup(
+    name="test-project",
+    version="0.0.1",
+    long_description="file:{readme_filename}",
+    long_description_content_type="{mime_type}"
+)
+"""
+
+    project.write(readme_filename, "Dummy README\n")
+    project.setup_py(setup_py)
+    result = project.generate()
+    assert result["project"]["readme"] == {"content-type": mime_type, "file": readme_filename}
+
+
+@parametrize_readme_type
 def test_file_without_content_type(project, extension: str, mime_type: str) -> None:
     # mime_type is unused but we keep it to be able to use the same parametrization as other tests
     readme_filename = f"README.{extension}"
@@ -94,5 +114,25 @@ long_description = file:{readme_filename}
     project.setup_cfg(setup_cfg)
     project.write(readme_filename, "Dummy README\n")
     project.setup_py()
+    result = project.generate()
+    assert result["project"]["readme"] == readme_filename
+
+
+@parametrize_readme_type
+def test_file_without_content_type_setuppy(project, extension: str, mime_type: str) -> None:
+    # mime_type is unused but we keep it to be able to use the same parametrization as other tests
+    readme_filename = f"README.{extension}"
+    setup_py = f"""\
+import setuptools
+
+setuptools.setup(
+    name="test-project",
+    version="0.0.1",
+    long_description="file:{readme_filename}"
+)
+"""
+
+    project.write(readme_filename, "Dummy README\n")
+    project.setup_py(setup_py)
     result = project.generate()
     assert result["project"]["readme"] == readme_filename
