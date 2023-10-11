cb1dd63df17efbac471f3e0e96f8281608dc6467
Stuart Longland <me@vk4msl.com>
2023-10-11 10:40:09 +1000

    issue 102: readme tests: Add further inline string tests for `setup.py`

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -41,6 +41,31 @@ long_description_content_type = {mime_type}
     assert readme["content-type"] == mime_type
 
 
+@parametrize_readme_type
+def test_string_with_content_type_setuppy(project, extension: str, mime_type: str) -> None:
+    long_description = "This is a long description string"
+    setup_py = f"""\
+import setuptools
+
+setuptools.setup(
+    name="test-project",
+    version="0.0.1",
+    long_description="{long_description}",
+    long_description_content_type="{mime_type}"
+)
+"""
+
+    project.setup_py(setup_py)
+    result = project.generate()
+    readme = result["project"]["readme"]
+    assert isinstance(readme, dict)
+    readme_path: pathlib.Path = pathlib.Path(readme["file"])
+    assert readme_path.suffix == f".{extension}"
+    assert readme_path.exists()
+    assert readme_path.read_text(encoding="utf-8").rstrip("\n") == long_description
+    assert readme["content-type"] == mime_type
+
+
 def test_string_without_content_type(project) -> None:
     long_description = "This is a long description string"
     setup_cfg = f"""\
@@ -52,6 +77,29 @@ long_description = {long_description}
 
     project.setup_cfg(setup_cfg)
     project.setup_py()
+    result = project.generate()
+    readme = result["project"]["readme"]
+    assert isinstance(readme, str)
+    readme_path: pathlib.Path = pathlib.Path(readme)
+    # Without a content type specified, there should be no extension given to the README file
+    assert not readme_path.suffix
+    assert readme_path.exists()
+    assert readme_path.read_text(encoding="utf-8").rstrip("\n") == long_description
+
+
+def test_string_without_content_type_setuppy(project) -> None:
+    long_description = "This is a long description string"
+    setup_py = f"""\
+import setuptools
+
+setuptools.setup(
+    name="test-project",
+    version="0.0.1",
+    long_description="{long_description}"
+)
+"""
+
+    project.setup_py(setup_py)
     result = project.generate()
     readme = result["project"]["readme"]
     assert isinstance(readme, str)
