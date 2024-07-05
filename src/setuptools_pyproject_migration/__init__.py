6eb3fc82c147732c835bf00ef8ee723520e1b2e6
David Zaslavsky <diazona@ellipsix.net>
2024-07-05 11:39:29 -0700

    Split out type declarations into a new module
    This commit moves type declarations into a new module, _types.py, which
    has two benefits:
    
    - It helps code organization and readability
    - It lets us use the types from different modules, when additional
      modules are added in the future, without having to deal with circular
      imports.

--- src/setuptools_pyproject_migration/__init__.py
+++ src/setuptools_pyproject_migration/__init__.py
@@ -7,21 +7,9 @@ import tomlkit
 import warnings
 from packaging.specifiers import SpecifierSet
 from pep508_parser import parser as pep508
+from setuptools_pyproject_migration._types import Contributor, Pyproject
 from tomlkit.api import Array, InlineTable
-from typing import Dict, List, Optional, Set, Tuple, Type, TypeVar, Union
-
-# After we drop support for Python <3.10, we can import TypeAlias directly from typing
-from typing_extensions import Required, TypedDict
-
-
-# PEP 518
-BuildSystem: Type = TypedDict("BuildSystem", {"requires": List[str], "build-backend": str}, total=True)
-
-# https://packaging.python.org/en/latest/specifications/declaring-project-metadata/
-Contributor: Type = TypedDict("Contributor", {"name": str, "email": str}, total=False)
-LicenseFile: Type = TypedDict("LicenseFile", {"file": str})
-LicenseText: Type = TypedDict("LicenseText", {"text": str})
-ReadmeInfo: Type = TypedDict("ReadmeInfo", {"file": str, "content-type": str})
+from typing import Dict, List, Optional, Set, Tuple, TypeVar, Union
 
 
 def _parse_entry_point(entry_point: str) -> Tuple[str, str]:
@@ -120,38 +108,6 @@ def _generate_entry_points(entry_points: Optional[Union[Dict[str, List[str]], st
 
     return parsed_entry_points
 
-
-Project: Type = TypedDict(
-    "Project",
-    {
-        "authors": List[Contributor],
-        "classifiers": List[str],
-        "dependencies": List[str],
-        "description": str,
-        "dynamic": List[str],
-        "entry-points": Dict[str, Dict[str, str]],
-        "gui-scripts": Dict[str, str],
-        "keywords": List[str],
-        "license": Union[LicenseFile, LicenseText],
-        "maintainers": List[Contributor],
-        "name": Required[str],
-        "optional-dependencies": Dict[str, List[str]],
-        "readme": Union[str, ReadmeInfo],
-        "requires-python": str,
-        "scripts": Dict[str, str],
-        "urls": Dict[str, str],
-        "version": str,
-    },
-    total=False,
-)
-
-Pyproject: Type = TypedDict("Pyproject", {"build-system": BuildSystem, "project": Project}, total=False)
-"""
-The type of the data structure stored in a ``pyproject.toml`` file. This only
-includes the build system and core metadata portions, i.e. the ``build-system``
-and ``project`` tables. The data structure may contain other entries that are
-not constrained by this type.
-"""
 
 T = TypeVar("T")
 
