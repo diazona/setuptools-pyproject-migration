c07dcced95ba3b4e4a5b485e2b5cf4d6784096a6
David Zaslavsky <diazona@ellipsix.net>
2024-07-29 03:33:38 -0700

    Mark optional dependency tests as passing
    With the changes from the previous commit to relax the check on extras
    in Requirement markers, these tests are now passing.

--- tests/distribution/test_distribution_packages.py
+++ tests/distribution/test_distribution_packages.py
@@ -72,7 +72,6 @@ distributions: List = [
         marks=[
             pytest.mark.distribute(
                 {
-                    "test_optional_dependencies": pytest.mark.xfail(reason="Issue #134"),
                     "test_authors": pytest.mark.xfail(reason="Issue #135"),
                 }
             ),
@@ -84,7 +83,6 @@ distributions: List = [
         marks=pytest.mark.distribute(
             {
                 "test_readme": pytest.mark.xfail,
-                "test_optional_dependencies": pytest.mark.xfail(reason="Issue #134"),
             }
         ),
     ),
